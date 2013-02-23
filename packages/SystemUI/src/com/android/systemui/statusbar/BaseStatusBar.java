5953f364420248b0f7d250766419a573ddad4685
drcmda <drcmda@gmail.com>
2013-02-23 21:53:57 +0000

    PIE: option for Pie to maintain its position, disregarding orientation changes (useful for gaming)
    Change-Id: I2bde8ee6f6fdff602a1241d4da7fd88ed3216477
    
    Conflicts:
    	core/java/android/provider/Settings.java
    	packages/SystemUI/src/com/android/systemui/statusbar/BaseStatusBar.java

--- packages/SystemUI/src/com/android/systemui/statusbar/BaseStatusBar.java
+++ packages/SystemUI/src/com/android/systemui/statusbar/BaseStatusBar.java
@@ -221,8 +221,6 @@ public abstract class BaseStatusBar extends SystemUI implements
         void observe() {
             ContentResolver resolver = mContext.getContentResolver();
             resolver.registerContentObserver(Settings.System.getUriFor(
-                    Settings.System.PIE_GRAVITY), false, this);
-            resolver.registerContentObserver(Settings.System.getUriFor(
                     Settings.System.PIE_TRIGGER), false, this);
             resolver.registerContentObserver(Settings.System.getUriFor(
                     Settings.System.EXPANDED_DESKTOP_STATE), false, this);
@@ -419,6 +417,18 @@ public abstract class BaseStatusBar extends SystemUI implements
                     userSwitched(mCurrentUserId);
                 }
             }}, filter);
+
+        // Listen for PIE gravity
+        mContext.getContentResolver().registerContentObserver(
+            Settings.System.getUriFor(Settings.System.PIE_GRAVITY), false, new ContentObserver(new Handler()) {
+                @Override
+                public void onChange(boolean selfChange) {
+                    if (Settings.System.getInt(mContext.getContentResolver(),
+                            Settings.System.PIE_STICK, 0) == 0) {
+                        updatePieControls();
+                    }
+                }
+            });
 
         attachPie();
 
